bea45c35f9c93a6e50c188ef40595a001c8f160b
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-12-16 23:10:05 +0900

    feat ($user) : user table 컬럼 수정 + status 기본값 Active로 설정 + datetime 기본값 서버 시간

--- api/table/user.py
+++ api/table/user.py
@@ -1,10 +1,13 @@
 from ..config import db
-
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=True)
+    status = db.Column(db.String(255), nullable=False, server_default='Active')
+    createAt = db.Column(db.dateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"User {self.id}: {self.username}"

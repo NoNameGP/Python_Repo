193aa4d453fa2ce114d49e5caccfeaaa8067b213
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-12-31 13:24:11 +0900

    feat : 클래스 분리 성공!

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # SQLite 데이터베이스
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-api = Api(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(255), nullable=False)
-
-    def __repr__(self):
-        return f"Todo {self.id}: {self.task}"
+from flask import request
+from flask_restful import Resource
+from database import db,api,app
+from model import User
 
 
 class TodoListResource(Resource):
     def get(self):
-        todos = Todo.query.all()
+        todos = User.query.all()
         todo_dict = {}
         for todo in todos:
             todo_dict[todo.id] = todo.task
@@ -28,14 +14,14 @@ class TodoListResource(Resource):
 
     def post(self):
         json_data = request.get_json()
-        if not json_data or 'task' not in json_data:
-            return {'error': 'Task is required'}, 400
+        if not json_data or 'user' not in json_data:
+            return {'error': 'User is required'}, 400
 
-        new_todo = Todo(task=json_data['task'])
-        db.session.add(new_todo)
+        new_user = User(email=json_data['email'])
+        db.session.add(new_user)
         db.session.commit()
 
-        return {'id': new_todo.id, 'task': new_todo.task}
+        return {'id': new_user.id, 'task': new_user.task}
 
 
 
--- model.py
+++ model.py
@@ -1,12 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from database import db
+from datetime import datetime
+from sqlalchemy.orm import relationship
 
-db = SQLAlchemy()
-migrate = Migrate(db)
 
-class Todo(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=True)
+    status = db.Column(db.String(255), nullable=False, server_default='Active')
+    createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    marks = relationship('Mark', back_populates='user')
+
+    def __init__(self,email):
+        self.email = email
 
     def __repr__(self):
-        return f"Todo {self.id}: {self.task}"
+        return f"User {self.id}: {self.username}"
+
+
+class Mark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = relationship('User', back_populates='marks')
+    name = db.Column(db.String(255), nullable=True)
+    endPoint = db.Column(db.Float)
+    status = db.Column(db.String(255), nullable=False, server_default='Active')
+    createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Mark {self.name}: {self.endPoint}"

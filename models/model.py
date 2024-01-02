d74bcd6d166812120d11071f33aefcb1b4401e18
Sasha <106591445+kchaeys2@users.noreply.github.com>
2024-01-02 11:30:16 +0900

    feat : Route 테이블 추가 + 유저에 비밀번호 추가

--- models/model.py
+++ models/model.py
@@ -8,9 +8,11 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), nullable=True)
+    password = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(255), nullable=False, server_default='Active')
     createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     marks = relationship('Mark', back_populates='user')
+    routes = relationship('Route',back_populates= 'user')
 
     def __init__(self, email):
         self.email = email
@@ -30,3 +32,18 @@ class Mark(db.Model):
 
     def __repr__(self):
         return f"Mark {self.name}: {self.endPoint}"
+
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    routeOwner = relationship('User', back_populates='routes')
+
+    startX = db.Column(db.Float)
+    startY = db.Column(db.Float)
+    endX = db.Column(db.Float)
+    endY = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"Route {self.user}: {self.startX},{self.startY}"
+

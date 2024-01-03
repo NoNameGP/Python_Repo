818ed09a14c476a6d68b577a71d2feab3c87bb47
Sasha <106591445+kchaeys2@users.noreply.github.com>
2024-01-03 23:55:47 +0900

    feat : passpoint,object table 추가 - endPoint -> + endX,endY로 변경

--- models/model.py
+++ models/model.py
@@ -12,7 +12,7 @@ class User(db.Model):
     status = db.Column(db.String(255), nullable=False, server_default='Active')
     createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     marks = relationship('Mark', back_populates='user')
-    routes = relationship('Route',back_populates= 'user')
+    routes = relationship('Route', back_populates='user')
 
     def __init__(self, email):
         self.email = email
@@ -26,12 +26,35 @@ class Mark(db.Model):
     userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = relationship('User', back_populates='marks')
     name = db.Column(db.String(255), nullable=False)
-    endPoint = db.Column(db.Float)
+    endX = db.Column(db.Float)
+    endY = db.Column(db.Float)
     status = db.Column(db.String(255), nullable=False, server_default='Active')
     createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"Mark {self.name}: {self.endPoint}"
+        return f"Mark {self.name}: {self.endX}"
+class Object(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    route = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
+    objectOwner = relationship('Route', back_populates='objects')
+
+    pointX = db.Column(db.Float)
+    pointY = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"Object {self.route}: {self.pointX},{self.pointY}"
+
+
+class PassPoint(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    route = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
+    passPointOwner = relationship('Route', back_populates='passpoints')
+
+    pointX = db.Column(db.Float)
+    pointY = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"PassPoint {self.route}: {self.pointX},{self.pointY}"
 
 
 class Route(db.Model):
@@ -44,6 +67,8 @@ class Route(db.Model):
     endX = db.Column(db.Float)
     endY = db.Column(db.Float)
 
-    def __repr__(self):
-        return f"Route {self.user}: {self.startX},{self.startY}"
+    objects = relationship('Object', back_populates='objects')
+    passpoints = relationship('PassPoint', back_populates='passpoints')
 
+    def __repr__(self):
+        return f"Route {self.user.id}: {self.startX},{self.startY}"

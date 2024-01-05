5f9d446289c3af9ea19cd6bc6a15c30163acdf60
Sasha <106591445+kchaeys2@users.noreply.github.com>
2024-01-05 15:02:28 +0900

    refactor($model) : baseModel 및 Coordinate 클래스 사용해 중복 제거

--- models/model.py
+++ models/model.py
@@ -5,16 +5,24 @@ from sqlalchemy.orm import relationship
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class BaseModel:
+    status = db.Column(db.String(255), nullable=False, server_default='Active')
+    createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class Coordinate:
+    X = db.Column(db.Float)
+    Y = db.Column(db.Float)
+
+
+class User(db.Model, BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), nullable=True)
     password = db.Column(db.String(255), nullable=False)
-    status = db.Column(db.String(255), nullable=False, server_default='Active')
-    createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     marks = relationship('Mark', back_populates='markOwner')
     routes = relationship('Route', back_populates='routeOwner')
 
-    def __init__(self, email,password):
+    def __init__(self, email, password):
         self.email = email
         self.password = password
 
@@ -22,43 +30,35 @@ class User(db.Model):
         return f"User {self.id}: {self.username}"
 
 
-class Mark(db.Model):
+class Mark(db.Model, BaseModel, Coordinate):
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     markOwner = relationship('User', back_populates='marks')
     name = db.Column(db.String(255), nullable=False)
-    endX = db.Column(db.Float)
-    endY = db.Column(db.Float)
-    status = db.Column(db.String(255), nullable=False, server_default='Active')
-    createAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"Mark {self.name}: {self.endX}"
-class Object(db.Model):
+
+
+class Object(db.Model, BaseModel,Coordinate):
     id = db.Column(db.Integer, primary_key=True)
     route = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
     objectOwner = relationship('Route', back_populates='objects')
-
-    pointX = db.Column(db.Float)
-    pointY = db.Column(db.Float)
 
     def __repr__(self):
         return f"Object {self.route}: {self.pointX},{self.pointY}"
 
 
-class PassPoint(db.Model):
+class PassPoint(db.Model, BaseModel,Coordinate):
     id = db.Column(db.Integer, primary_key=True)
     route = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
     passPointOwner = relationship('Route', back_populates='passPoints')
-
-    pointX = db.Column(db.Float)
-    pointY = db.Column(db.Float)
 
     def __repr__(self):
         return f"PassPoint {self.route}: {self.pointX},{self.pointY}"
 
 
-class Route(db.Model):
+class Route(db.Model, BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     routeOwner = relationship('User', back_populates='routes')
